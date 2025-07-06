0717c3fc1844698d50ae0e6d802603359b6f146d
Siddharth Panchal <1234siddharth.panchal@gmail.com>
2025-07-06 14:13:52 +0530

    Made Attendance Tracker

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Button className="bg-blue-500 text-white">Hello</Button>
+    </div>
+    </>
+  );
+}
